7680288a671b5f88cd667c9f9a37e71f0f9fa303
sky8492002 <sky8492002@gmail.com>
2023-12-21 02:31:07 +0900

    feat: custom paging을 사용하는 viewmodel 구성

--- app/src/main/java/com/choi/sensorproject/ui/ShowRecordViewModel.kt
+++ app/src/main/java/com/choi/sensorproject/ui/ShowRecordViewModel.kt
@@ -1,12 +1,53 @@
 package com.choi.sensorproject.ui
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.choi.sensorproject.domain.paging.CustomPagingSource
 import com.choi.sensorproject.domain.usecase.GetSensorRecordsUseCase
+import com.choi.sensorproject.ui.model.RecordsForHourModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ShowRecordViewModel  @Inject constructor(
-    private val getSensorRecordsUseCase: GetSensorRecordsUseCase
-){
+    private val customPagingSource: CustomPagingSource
+): ViewModel(){
 
+    private val _uiState =
+        MutableStateFlow<UIState>(UIState.Success(PagingData.empty()))
+    val uiState: StateFlow<UIState> = _uiState
+
+    fun getRecords() : Flow<PagingData<RecordsForHourModel>> {
+        return Pager(config = PagingConfig(pageSize = 20, enablePlaceholders = false, initialLoadSize = 20, ), pagingSourceFactory = {
+            customPagingSource
+        }).flow
+    }
+
+    init{
+        viewModelScope.launch() {
+            try {
+                getRecords().collect {
+                    _uiState.value = UIState.Success(it)
+                }
+            }
+            catch (e: Exception){
+                _uiState.value = UIState.Fail(e)
+            }
+        }
+    }
+}
+
+sealed class UIState {
+    data class Success(val records: PagingData<RecordsForHourModel>) : UIState()
+
+    data class Fail(val error: Exception) : UIState()
 }
