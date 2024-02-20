5cc326d5e54ca8cb921ffa3c6a9aba2d8920bdd2
sky8492002 <sky8492002@gmail.com>
2024-02-21 03:13:53 +0900

    perf: 시간별 분류 과정 개선 후 앱 실행 시 pin 이동이 버벅이는 문제 해결 (timeFormat.parse를 사전에 실행하여 캐싱하지 않으면 pin 이동 시 많은 시간이 소모되므로 분류 과정 중 비동기로 실행)

--- app/src/main/java/com/choi/sensorproject/data/mapper/ToModel.kt
+++ app/src/main/java/com/choi/sensorproject/data/mapper/ToModel.kt
@@ -1,13 +1,17 @@
 package com.choi.sensorproject.data.mapper
 
 import android.annotation.SuppressLint
+import android.util.Log
 import com.choi.sensorproject.domain.model.AppInfoModel
 import com.choi.sensorproject.domain.model.RecordsForHourModel
 import com.choi.sensorproject.domain.model.SensorRecordModel
 import com.choi.sensorproject.data.room.entity.AppInfoEntity
 import com.choi.sensorproject.data.room.entity.SensorRecordEntity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
 
 fun SensorRecordEntity.toModel(): SensorRecordModel {
@@ -28,20 +32,27 @@ fun List<SensorRecordModel>.toRecordsForHourModels(pageDate: String): List<Recor
     @SuppressLint("SimpleDateFormat")
     val hourFormat = SimpleDateFormat("HH")
 
+    val sensorRecordModels = this
     val recordsForHourModels: MutableList<RecordsForHourModel> = mutableListOf()
+
     for (hour in 0 until 24) {
         val emptyRecords: MutableList<SensorRecordModel> = mutableListOf()
         recordsForHourModels.add(RecordsForHourModel(pageDate, hour.toString(), emptyRecords))
     }
 
+    // 여기서 timeFormat.parse(sensorRecordUIModel.recordTime)을 하는 것이 customClockView에서 시점을 터치할 때의 버벅임을 줄여줌
+    // 동일한 함수를 호출할 때 캐시에 저장된 데이터를 사용하기 때문
+    // 기록 로딩 속도에 영향을 주지 않기 위해 비동기로 실행
+    CoroutineScope(Dispatchers.IO).launch {
+        for (sensorRecordModel in sensorRecordModels) {
+            timeFormat.parse(sensorRecordModel.recordTime)
+        }
+    }
+
     // SensorRecordModel list의 요소를 시간 별로 통합하여 RecordsForHourModel list로 변환
-    for (sensorRecordModel in this) {
+    for (sensorRecordModel in sensorRecordModels) {
         val hour = sensorRecordModel.recordTime.split(" ")[1].split(":")[0].toInt()
         recordsForHourModels[hour].records.add(sensorRecordModel)
-//        timeFormat.parse(sensorRecordModel.recordTime)?.let {
-//            val hour = hourFormat.format(it).toInt()
-//            recordsForHourModels[hour].records.add(sensorRecordModel)
-//        }
     }
 
     return recordsForHourModels.toList()
